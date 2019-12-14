a2ef6617871ab523829e7262590313741f80fa51
Jingle <2521019626@qq.com>
2019-12-14 15:40:48 +0800

    增加抽屉

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,35 +3,69 @@
     <Header>掌上市政平台</Header>
     <div id="viewDiv" ref="map"></div>
     <div class="sliderbar">
-      <div class="slider" @click="monitorType"></div>
-      <div class="slider"></div>
+      <div class="slider" @click="monitorType">
+        <img src="/imgs/map-type.png" alt="">
+      </div>
+      <div class="slider">
+        <img src="/imgs/report.png" alt="">
+      </div>
     </div>
     <mt-popup v-model="popupVisible" position="right" class="monitor-popup">
       <MonitorPopup>地图类型</MonitorPopup>
       <div class="map-type">
-        <div style="height:65px;width:65px;">
-          <img src="/imgs/line.png" alt="" style="width:100%;">
+        <div class="map-photo" :class="mapActive?'active':''" @click="()=>{mapActive=true}">
+          <img src="/imgs/line.png" alt />
+          <img src="/imgs/map-active.png" alt class="map-active" />
+          <span>管线</span>
+        </div>
+        <div class="map-photo" :class="mapActive?'':'active'" @click="()=>{mapActive=false}">
+          <img src="/imgs/road.png" alt />
+          <img src="/imgs/map-active.png" alt class="map-active" />
+          <span>道路</span>
         </div>
       </div>
       <MonitorPopup>监测点</MonitorPopup>
+      <div class="map-type map-monitor">
+        <div
+          class="map-photo"
+          v-for="(data,index) in datalist"
+          :key="index"
+          :class="index===mapActiveIndex?'active':''"
+          @click="mapActiveClick(index)"
+        >
+          <img :src="data.imgs" alt />
+          <img src="/imgs/map-active.png" alt class="map-active" />
+          <span>{{data.name}}</span>
+        </div>
+      </div>
     </mt-popup>
   </div>
 </template>
  
 <script>
-import Vue from "vue";
 import Header from "../components/Header";
 import esriLoader from "esri-loader";
-import MonitorPopup from './Home/monitor-popup'
+import MonitorPopup from "./Home/monitor-popup";
 export default {
   data() {
     return {
-      popupVisible: true
+      popupVisible: false,
+      mapActive: false,
+      datalist: [
+        { name: "泵站", imgs: "/imgs/pumping.png" },
+        { name: "雨污混排", imgs: "/imgs/sewage.png" },
+        { name: "积水", imgs: "/imgs/rain.png" },
+        { name: "井盖", imgs: "/imgs/manhole.png" }
+      ],
+      mapActiveIndex: 0
     };
   },
   methods: {
     monitorType() {
       this.popupVisible = true;
+    },
+    mapActiveClick(index) {
+      this.mapActiveIndex = index;
     }
   },
   mounted() {
@@ -80,24 +114,67 @@ export default {
   right: 0.2rem;
   top: 1rem;
   .slider {
-    width: 0.96rem;
-    height: 0.96rem;
-    background: rgba(255, 255, 255, 1);
-    box-shadow: 0px 3px 10px rgba(228, 239, 251, 1);
-    opacity: 1;
-    border-radius: 0.48rem;
-    margin-top: 0.2rem;
+    img{
+      width:1.5rem;
+    }
   }
 }
-.monitor-popup{
-  width:4.76rem;
-  height:calc(100vh - 1.84rem);
-  background:white;
-  top:49%;
-  .map-type{
-    height:2.2rem;
-    padding-top:0.4rem;
-    box-sizing:border-box;
+.monitor-popup {
+  width: 4.76rem;
+  height: calc(100vh - 1.84rem);
+  background: white;
+  top: 49%;
+  .map-type {
+    height: 2.2rem;
+    padding-top: 0.4rem;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    .map-photo {
+      position: relative;
+      width: 1.3rem;
+      border-radius: 7px;
+      text-align: center;
+      img:first-child {
+        width: 1.3rem;
+        display: block;
+        margin: 0 auto;
+      }
+      .map-active {
+        position: absolute;
+        display: none;
+        bottom: 0.6rem;
+        left: 50%;
+        transform: translateX(-50%);
+      }
+      span {
+        color: rgba(163, 165, 167, 1);
+        position: relative;
+        top: 0.1rem;
+      }
+    }
+    .active {
+      .map-active {
+        display: block;
+      }
+      span {
+        color: black;
+      }
+    }
+  }
+  .map-monitor {
+    height: auto;
+    padding: 0;
+    .map-photo {
+      width: 50%;
+      margin-top: 0.4rem;
+    }
+    .active {
+      .map-active {
+        bottom: 0.5rem;
+      }
+    }
   }
 }
 </style>
