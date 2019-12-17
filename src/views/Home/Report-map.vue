7358c81d43f874c00fac4a9214fd8b88fd839286
Jingle <2521019626@qq.com>
2019-12-17 18:18:54 +0800

    详情描述

--- src/views/Home/Report-map.vue
+++ src/views/Home/Report-map.vue
@@ -2,11 +2,29 @@
   <div>
     <Header>上报</Header>
     <div id="viewDiv" ref="map"></div>
-    <div class="problem">
-      <div class="problem-title">{{name}}</div>
-      <div class="problem-content">
-        <p>{{name}}问题</p>
-        <p>在地图确定问题位置</p>
+    <div
+      class="report-hide"
+      :style="listShow?'transform:translateY(70%)':'transform:translateY(0)'"
+    >
+      <div class="problem">
+        <div class="problem-title">{{name}}</div>
+        <div class="problem-content">
+          <p>{{name}}问题</p>
+          <p>{{area}}</p>
+        </div>
+        <div class="report-update" @click="reportPut" v-if="!listShow">
+          <img src="/imgs/report-update.png" alt />
+          <span>更改位置</span>
+        </div>
+      </div>
+      <div class="report-describe">
+        <p class="describe-text">详情描述</p>
+      </div>
+    </div>
+    <div ref="chatBox" class="chat">
+      <div class="chat-box">
+        <span>浙江省湖州市德清县科源路辅路</span>
+        <p @click="determine">确定</p>
       </div>
     </div>
   </div>
@@ -15,17 +33,30 @@
 <script>
 import Header from "../../components/Header";
 import map from "./js/map";
-import {point} from './js/point'
+import { point } from "./js/point";
 export default {
-  data () {
-		return {
-			name:""
-		}
-	},
+  data() {
+    return {
+      name: "",
+      area: "在地图确定问题位置",
+      listShow: false
+    };
+  },
+  methods: {
+    determine() {
+      this.listShow = false;
+      this.area = "浙江省湖州市德清县科源路辅路";
+      this.$refs.chatBox.style.display = "none";
+    },
+    reportPut() {
+      this.listShow = true;
+      this.area = "在地图确定问题位置";
+    }
+  },
   mounted() {
     this.$store.commit("commitShow", false);
-    this.name = this.$route.query.name
-    point()
+    this.name = this.$route.query.name;
+    point(this.$refs.chatBox);
   },
   destroyed() {
     this.$store.commit("commitShow", true);
@@ -40,42 +71,93 @@ export default {
 #viewDiv {
   position: absolute;
   top: 0.8rem;
+  left: 0;
   height: calc(100vh - 2.4rem);
   width: 100%;
 }
-.problem {
-  height: 1.6rem;
+.report-hide {
+  position: fixed;
   width: 100%;
-  background: white;
-  position: absolute;
+  height: 5.5rem;
   bottom: 0;
-  left: 0;
-  z-index: 2;
+  background: white;
+  padding: 0.3rem 0.3rem 0 0.3rem;
   box-sizing: border-box;
-  padding: 0.3rem 0 0 0.3rem;
-  display: flex;
-  .problem-title {
-    width: 0.9rem;
-    height: 0.9rem;
-    border: 2px solid rgba(50, 150, 250, 1);
-    border-radius: 50%;
-    color: rgba(50, 150, 250, 1);
-    line-height: 0.9rem;
-    text-align: center;
-		margin-right: 0.22rem;
-		overflow: hidden;
-  }
-  .problem-content {
-    height:0.94rem;
+  transition: 0.3s;
+  .problem {
+    width: 100%;
+    height: 1.4rem;
+    background: white;
+    position: relative;
     box-sizing: border-box;
-    padding-top:0.1rem;
-    p:nth-of-type(1) {
-      color: black;
-      font-weight:bold;
+    border-bottom: 1px solid rgba(112, 112, 112, 1);
+    .problem-title {
+      width: 0.9rem;
+      height: 0.9rem;
+      border: 2px solid rgba(50, 150, 250, 1);
+      border-radius: 50%;
+      color: rgba(50, 150, 250, 1);
+      line-height: 0.9rem;
+      text-align: center;
+      margin-right: 0.22rem;
+      overflow: hidden;
+      float: left;
     }
-    p:nth-of-type(2) {
+    .problem-content {
+      height: 0.94rem;
+      box-sizing: border-box;
+      padding-top: 0.1rem;
+      float: left;
+      p:nth-of-type(1) {
+        color: black;
+        font-weight: bold;
+      }
+      p:nth-of-type(2) {
+        font-size: 0.28rem;
+        color: rgba(25, 31, 37, 1);
+      }
+    }
+    .report-update {
+      padding-top: 0.46rem;
+      color: rgba(50, 150, 250, 1);
       font-size: 0.28rem;
-      color: rgba(25, 31, 37, 1);
+      float: right;
+      img {
+        height: 0.28rem;
+        position: relative;
+        top: 0.05rem;
+      }
+    }
+  }
+  .report-describe {
+    .describe-text {
+      line-height:1rem;
+      color:rgba(25, 31, 37, 1);
+    }
+  }
+}
+.chat {
+  display: none;
+  position: absolute;
+  width: 5.5rem;
+  .chat-box {
+    width: 100%;
+    height: 0.8rem;
+    background: url("/imgs/chat-box.png") no-repeat;
+    display: flex;
+    line-height: 0.8rem;
+    padding-left: 0.4rem;
+    font-size: 0.28rem;
+    p {
+      height: 0.4rem;
+      position: relative;
+      top: 0.2rem;
+      margin-left: 0.2rem;
+      padding-left: 0.2rem;
+      line-height: 0.4rem;
+      border-left: 1px solid black;
+      color: RGBA(50, 150, 250, 1);
+      font-weight: bold;
     }
   }
 }
--- src/views/Home/js/point.js
+++ src/views/Home/js/point.js
@@ -1,6 +1,6 @@
 import esriLoader from "esri-loader";
 import map from './map'
-function point() {
+function point(node) {
   var esri = map()
   esri.then(res => {
     var map = res.map;
@@ -33,86 +33,34 @@ function point() {
 
       var pointGraphic = new Graphic({
         geometry: point,
-        symbol: markerSymbol,
-        attributes: {
-          title: '正常设备',
-          EquipmentNumber: 'JG002',
-          HoleNumber: 'JG002',
-          Company: '德清市政',
-          Manufacturer: '浙江正元地理信息有限责任公司',
-          Installer: '浙江正元地理信息有限责任公司',
-          date: '2019/01/01',
-          state: '正常'
-        }
+        symbol: markerSymbol
       });
       graphicsLayer.add(pointGraphic);
-      view.on('hold', e => {
+      view.on('click', e => {
         view.hitTest(e).then(res => {
-          const graphic = res.results[0].graphic
-          var a = view.popup.open({
-            location: graphic.geometry,
-            content: report(graphic),
-          });
-          console.log(a)
+          if (res.results.length) {
+            node.style.display = "block"
+            node.style.left = res.screenPoint.x + 'px'
+            node.style.transform = "translate(-43%,-30%)"
+            node.style.top = res.screenPoint.y + 'px'
+          } else {
+            node.style.display = "none"
+          }
         })
-        // view.popup.open({
-        //   title: "hahha",
-        //   content: "aaaaaaaaa"  // content displayed in the popup
-        // });
       })
+      // view.on('drag', e => {
+      //   view.hitTest(e).then(res => {
+      //     if (node.style.display === "block") {
+      //       console.log(res)
+      //       node.style.left = res.screenPoint.x + 'px'
+      //       node.style.top = res.screenPoint.y + 'px'
+      //     }
+      //   })
+      // })
     })
   })
 }
 
-function report(graphic) {
-  return `<div id="mydiv">
-    <div id="h1" style="background:linear-gradient(-90deg,rgba(176,225,241,1) 0%,rgba(15,156,149,1) 100%)">
-      <span id="title">${graphic.attributes['title']}</span>
-    </div>
-    <div class="row">
-      <div class="left">
-        <span id="11" class="title1"> 设备编号:&nbsp&nbsp</span>
-        <span id="value11" class="value1"> ${graphic.attributes['EquipmentNumber']}</span>
-        </div>              
-    </div>
-    <div class="row">
-      <div class="left">
-        <span id="11" class="title1">井盖编号:&nbsp&nbsp</span>
-        <span id="value11" class="value1"> ${graphic.attributes['HoleNumber']}</span>
-        </div>              
-    </div>
-    <div class="row">
-      <div class="left">
-        <span id="11" class="title1">管理单位:&nbsp&nbsp</span>
-        <span id="value11" class="value1"> ${graphic.attributes['Company']}</span>
-        </div>              
-    </div>
-    <div class="row change" >
-      <div class="left">
-        <span id="11" class="title1">生产厂家:&nbsp&nbsp</span>
-        <span id="value11" class="content">${graphic.attributes['Manufacturer']}</span>
-        </div>              
-    </div>
-    <div class="row change" >
-      <div class="left">
-        <span id="11" class="title1"> 安装单位:&nbsp&nbsp</span>
-        <span id="value11" class="content"> ${graphic.attributes['Installer']}</span>
-        </div>              
-    </div>
-    <div class="row">
-      <div class="left">
-        <span id="11" class="title1"> 安装日期:&nbsp&nbsp</span>
-        <span id="value11" class="value1"> ${graphic.attributes['date']}</span>
-        </div>              
-    </div>
-    <div class="row change1">
-      <div class="left">
-        <span id="11" class="title1"> 目前状态:&nbsp&nbsp</span>
-        <span id="value11" class="value1"> ${graphic.attributes['state']}</span>
-        </div>              
-    </div>
-  </div>`
-}
 export {
   point
 }
